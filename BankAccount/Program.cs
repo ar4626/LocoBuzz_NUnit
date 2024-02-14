ae8a67e6d093ef6d1b3148033342d3aebe18ed5e
Ankit Raj <ankitsingh17072001@gmail.com>
2024-02-14 10:51:00 +0530

    [Ankit] Add: Added the unit testing for a BankAccount Class

---
+++ BankAccount/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class BankAccounts
+    {
+        private double balance;
+        public BankAccounts() { }
+
+        public BankAccounts(double balance)
+        {
+            this.balance = balance;
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+        }
+
+        public void Add(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            this.balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > balance)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            if(amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            this.balance -= amount;
+        }
+
+        public void transferToOtherAccount(BankAccounts otherAccount, double amount)
+        {
+            if(otherAccount is null)
+            {
+                throw new ArgumentNullException(nameof(otherAccount));
+            }
+
+            Withdraw(amount);
+            otherAccount.Add(amount);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ BankAccountNUnitTest/BankAccountTest.cs
@@ -0,0 +1,48 @@
+namespace BankAccount.NUnitTest
+{
+    public class BankAccountTest
+    {
+        private BankAccounts account = null;
+        [SetUp]
+        public void Setup()
+        {
+            account = new BankAccounts();
+        }
+
+        [TestCase(500)]
+        public void Adding_Funds_Updates_Balance(int amt)
+        {
+            //ARRANGE
+            account = new BankAccounts(1000);
+
+            //ACT
+            account.Add(amt);
+
+            //ASSERT
+            Assert.AreEqual(1500, account.Balance);
+        }
+        [Test]
+        public void Withdraw_Funds_Updates_Balance()
+        {
+            //ARRANGE
+            account = new BankAccounts(1000);
+
+            //ACT
+            account.Withdraw(500);
+
+            //ASSERT
+            Assert.AreEqual(500, account.Balance);
+        }
+
+        [Test]
+        public void Transfer_Funds_To_Other_Account()
+        {
+            account = new BankAccounts(1000);
+            var otherAccount = new BankAccounts();
+
+            account.transferToOtherAccount(otherAccount, 500);
+            Assert.AreEqual(500, account.Balance);
+            Assert.AreEqual(500, otherAccount.Balance);
+        }
+    }
+}
